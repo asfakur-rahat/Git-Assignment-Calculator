5ad59e2f85570fa5568a47c25613cbc6def476a7
Arittrabs1528 <arittra.das@brainstation-23.com>
2024-04-26 14:23:58 +0600

    Added Multiplication feature

--- calculator.cpp
+++ calculator.cpp
@@ -24,5 +24,6 @@ long long addition(long long number1, long long number2){
         return number1 + number2;
 }
 long long multiplication(long long number1,long long number2){
+    
     return number1*number2;
 }
