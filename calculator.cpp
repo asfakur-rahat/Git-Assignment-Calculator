682d2eba0d79f076c16d5c17435c8b935ad73e4e
Fahim Mohammod Fardous <fahim.fardous@brainstation-23.com>
2024-04-26 14:23:39 +0600

    Added subtraction functionality in calculator file

--- calculator.cpp
+++ calculator.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 long long addition(long long number1, long long number2);
+long long subtraction(long long number1, long long number2);
 
 int main(){
     long long number1, number2;
@@ -22,4 +23,8 @@ int main(){
 
 long long addition(long long number1, long long number2){
         return number1 + number2;
+}
+
+long long subtraction(long long number1, long long number2){
+    return number1 - number2;
 }
